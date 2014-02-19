50e52bb19685e6f15e2bcb6fb4a2ddf18ac6f6bd
Piotrek Okoński <piotrek@okonski.org>
2014-02-19 13:46:11 +0800

    Restore initialize on precompile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,6 @@ Gallery::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
-  config.initialize_on_precompile = false
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
