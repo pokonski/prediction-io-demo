38990019993ed1b0f3fb2c8eb216d31918f63c58
Piotrek Okoński <piotrek@okonski.org>
2014-02-19 14:38:19 +0800

    Display debug recommendation info.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def similar_photos
+    begin
+      PREDICTIONIO.identify(current_user.uid)
+      PREDICTIONIO.get_itemsim_top_n("similarity", @person.uid, 5)
+    rescue PredictionIO::Client::ItemSimNotFoundError => e
+      []
+    end
+  end
+
+  def recommended_photos
+    begin
+      PREDICTIONIO.identify(current_user.uid)
+      PREDICTIONIO.get_itemrec_top_n("rec", 10)
+    rescue PredictionIO::Client::ItemRecNotFoundError => e
+      []
+    end
+  end
 end
